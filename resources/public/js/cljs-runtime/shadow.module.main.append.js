17ce4946969f3abe978ee9f547add76edb2a9cf5
Kanwei Li <kanwei@gmail.com>
2025-09-06 19:58:39 -0400

    Untrack build files

--- resources/public/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { puerto_rico.core.init(); } catch (e) { console.error("An error occurred when calling (puerto-rico.core/init)"); console.error(e); }
